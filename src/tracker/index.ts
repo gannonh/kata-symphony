207b30b17cf6afd8746b9337a86c9e15e8714ac0
Gannon Hall <gannonhall@gmail.com>
2026-03-05 17:22:54 -0800

    fix(tracker): stop re-exporting linear GraphQL types
    Remove low-level Linear GraphQL type re-exports from the tracker index
    so consumers rely on the adapter and normalization contract surface.

--- src/tracker/index.ts
+++ src/tracker/index.ts
@@ -1,14 +1,4 @@
 export * from './errors.js'
-export type {
-  LinearGraphQLError,
-  LinearIssueConnection,
-  LinearIssueNode,
-  LinearIssueRelationIssueNode,
-  LinearIssueRelationNode,
-  LinearLabelNode,
-  LinearPageInfo,
-  LinearStateNode,
-} from './linear/types.js'
 export * from './linear/client.js'
 export * from './linear/http.js'
 export * from './linear/normalize.js'
